09aeaf32848130b4107728bd91272b17069062db
Hamm-JH <52693438+Hamm-JH@users.noreply.github.com>
2021-12-04 19:57:08 +0900

    Update 1204

--- Assets/02.Script/JH/Manager/GameManager.cs
+++ Assets/02.Script/JH/Manager/GameManager.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 namespace Management
 {
+	/// <summary>
+	/// 관리자 클래스의 부모 클래스
+	/// </summary>
 	public class GameManager : MonoBehaviour
 	{
-
+		// Fade in, Fade out 구현
 
 		// Start is called before the first frame update
 		void Start()
