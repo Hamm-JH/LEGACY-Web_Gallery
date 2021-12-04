b5e275d582a909977498c7a0fbe775fbb2689865
Hamm-JH <52693438+Hamm-JH@users.noreply.github.com>
2021-12-04 22:42:49 +0900

    Update 1204

---
+++ Assets/02.Script/JH/Manager/Core/CoreResource.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Management.Core
+{
+	[System.Serializable]
+	public class CoreResource
+	{
+		public string data;
+	}
+}
--- Assets/02.Script/JH/Manager/GameManager.cs
+++ Assets/02.Script/JH/Manager/GameManager.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 namespace Management
 {
+	using Core;
+
 	/// <summary>
 	/// 관리자 클래스의 부모 클래스
 	/// </summary>
-	public class GameManager : MonoBehaviour
+	public class GameManager : Manager
 	{
+		public CoreResource core;
+
 		// Fade in, Fade out 구현
 
 		// Start is called before the first frame update
