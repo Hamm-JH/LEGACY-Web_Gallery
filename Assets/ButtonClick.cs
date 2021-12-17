8aea239466670c071e93729b996f1a2927e4d168
Duruphil <53423341+Duruphil@users.noreply.github.com>
2021-12-17 19:26:30 +0900

    info canvas and modeling texture

---
+++ Assets/ButtonClick.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonClick : MonoBehaviour
+{
+    bool isClicked;
+    public GameObject image;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isClicked = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+  public void Button_Click()
+    {
+       
+        if(isClicked)
+        {
+            image.SetActive(true);
+            isClicked = false;
+        }
+        else
+        {
+            image.SetActive(false);
+            isClicked = true;
+        }
+       
+    }
+}
